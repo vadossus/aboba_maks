457e7f1d103ea86ad241cec3f58b99a978365c32
vadossus <157449023+vadossus@users.noreply.github.com>
2024-02-28 11:17:48 +0500

    Add files via upload

---
+++ Monster.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Monster
+    {
+        public string name;
+        int hp;
+        int damage;
+        int critical_damage;
+        static Random r = new Random();
+
+        static string[] harast = new string[6] { "Добрый", "Славный", "Кусачий", "Злой", "Бешеный","Тупой" };
+        static string[] names = new string[6] { "Гоблин", "Рыцарь", "Злыдень", "Водяной", "Воин","Петух" };
+
+        public void Create_name()
+        {
+            name = names[r.Next(6)] + " " + harast[r.Next(6)];
+        }
+        public int get_hp()
+        {
+            return hp;
+        }
+        public void set_hp()
+        {
+            hp = r.Next(50, 100);
+        }
+
+        public int Damage()
+        {
+            damage = r.Next(10, 20);
+            return damage;
+        }
+
+        public string DrawMonster()
+        {
+            return $"+----------{name}\n| Здоровье: {hp}\n| Урон: {damage}\n+----------";
+        }
+
+        public int Fight(int damage)
+        {
+            int total_damage = damage + critical_damage;
+            hp = hp - total_damage;
+            return hp;
+        }
+
+        public int CriticalDamage()
+        {
+            critical_damage = r.Next(0, 3);
+            critical_damage = damage * critical_damage;
+            return critical_damage;
+        }
+
+        public int Promax()
+        {
+            damage = 0;
+            critical_damage = 0;
+            hp = hp;
+            Console.WriteLine("Промахнулся");
+            return damage;
+            
+        }
+
+        public int Random()
+        {
+            int chance = r.Next(1, 3);
+            return chance;
+        }
+
+        public int Total()
+        {
+            int total = damage + critical_damage;
+            return total;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static Random r = new Random();
+        static void Main(string[] args)
+        {
+            Monster monster = new Monster();
+            Monster monster2 = new Monster();
+            Console.WriteLine("Добро пожаловать в игру: Монстер!");
+            Console.WriteLine("Ваша задача, отпинать другого монстра");
+            Console.WriteLine("Ваш персонаж: ");
+            monster.Create_name();
+            monster.set_hp();
+            monster.get_hp();
+            monster.Damage();
+            Console.WriteLine(monster.DrawMonster());
+
+            Console.WriteLine("Чужак: ");
+            monster2.Create_name();
+            monster2.set_hp();
+            monster2.get_hp();
+            monster2.Damage();
+            Console.WriteLine(monster2.DrawMonster());
+
+            Console.WriteLine("Хотите начать бой?: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            if (b == 1)
+            {
+                Console.WriteLine("Битва началась!");
+                int i = 0;
+                while (monster.get_hp() > 0 && monster2.get_hp() > 0)
+                {
+                    Console.WriteLine("----------------");
+                    Console.WriteLine($"Здоровье персонажа {monster.name}: {monster.Fight(monster2.Total())}");
+                    monster.Random();
+                    if (monster.Random() == 1 || monster.Random() == 2)
+                    {
+                        Console.WriteLine($"Ваш урон {monster.Damage()} c критическим уроном {monster.CriticalDamage()}");
+                        Console.WriteLine($"Итого: {monster.Total()}");
+                    }   
+                    else
+                    {
+                        monster.Promax();
+                    }
+                    Console.WriteLine("----------------");
+                    Console.WriteLine(" ");
+                    Thread.Sleep(3000);
+                    Console.WriteLine(" ");
+                    Console.WriteLine("----------------");
+                    Console.WriteLine($"Здоровье чужака {monster2.name}: {monster2.Fight(monster.Total())}");
+                    monster2.Random();
+                    if (monster2.Random() == 1 || monster2.Random() == 2)
+                    {
+                        Console.WriteLine($"Урон чужака: {monster2.Damage()} с критическим уроном {monster2.CriticalDamage()}");
+                        Console.WriteLine($"Итого: {monster2.Total()}");
+                    }                
+                    else
+                    {
+                        monster2.Promax();
+                    }
+                    Console.WriteLine("----------------");
+
+                    
+                }
+                Console.WriteLine("Конец!");
+            }
+            else
+            {
+                Console.WriteLine("До свидания");
+                Environment.Exit(0);
+            }
+
+            Console.Read();
+        }
+
+        
+    }
+}
